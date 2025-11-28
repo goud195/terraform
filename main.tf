477f8948f1e2de74dbd6864c117f6aae4f9c4bee
Govardhan <k.govardhangoud43@gmail.com>
2025-11-28 13:22:57 +0530

    Fix module path and Jenkinsfile

--- main.tf
+++ main.tf
@@ -2,11 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-module "data_library_lambda" {
-  source      = "./module/lambda"
-  environment = var.environment
-
-  # optional overrides, or you just use defaults in module
-  # runtime  = "python3.9"
-  # handler  = "index.handler"
+module "lambda_functions" {
+  source      = "./module/lambda"   # folder where your module code is
+  environment = var.environment     # dev / test / uat
 }
